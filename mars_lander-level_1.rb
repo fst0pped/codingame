c0f175f4d730535c3fb236d46401789906761704
Andrew <andy_dragonattack@hotmail.com>
2015-01-05 12:01:50 +0000

    Update mars_lander-level_1.rb
    Started again with actual landing formula. Removed coordinates stuff since it wasn't needed.
    
    Little bit hacky, but gets the economy achievement.

--- mars_lander-level_1.rb
+++ mars_lander-level_1.rb
@@ -2,15 +2,16 @@ STDOUT.sync = true # DO NOT REMOVE
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-$coordinates = []
-
 $N = gets.to_i # the number of points used to draw the surface of Mars.
 $N.times do
     # LAND_X: X coordinate of a surface point. (0 to 6999)
     # LAND_Y: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-#    $LAND_X, $LAND_Y = gets.split(" ").collect {|x| x.to_i}
-#    $coordinates << Array.new([$LAND_X, $LAND_Y])
+    $LAND_X, $LAND_Y = gets.split(" ").collect {|x| x.to_i}
 end
+
+max_thrust = 4
+g = 3.711
+max_deceleration = max_thrust - g
 
 # game loop
 loop do
@@ -21,14 +22,27 @@ loop do
     # P: the thrust power (0 to 4).
     $X, $Y, $HS, $VS, $F, $R, $P = gets.split(" ").collect {|x| x.to_i}
     
-    if $VS < -35
+    ticks_until_impact = $Y  / ($VS + 0.01)
+    time_to_decelerate = ($VS + 30) / max_deceleration # don't know why +30 and not +40 - perhaps because I'm calculating for the current tick rather than the next one.
+    
+    
+    if  time_to_decelerate <= ticks_until_impact
         $R = 4
     end
+
+ #       if $VS < -35 && $VS > -39
+ #           $R = 2
+ #       elsif $VS == -39
+ #           $R = 3
+ #       elsif $VS <= -40
+ #           $R = 4
+ #       end
+
     
     # Write an action using puts
     # To debug: STDERR.puts "Debug messages..."
     
-    STDERR.puts 
+    STDERR.puts max_deceleration
     
-    puts "-0 " + $R.to_s # R P. R is the desired rotation angle. P is the desired thrust power.
+    puts "0 " + $R.to_s # R P. R is the desired rotation angle. P is the desired thrust power.
 end
